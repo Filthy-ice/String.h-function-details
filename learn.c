4ddc0a7802035c92bf9550f82f5ed46bb99d4ec9
Filthy-ice <345134522@qq.com>
2022-07-16 21:44:44 +0800

    07.16night-up to date

--- learn.c
+++ learn.c
@@ -1,3 +1,38 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 
 #include <stdio.h>
+#include <string.h>
+#include <assert.h>
+
+//char* my_strcpy(char* dest, const char* src)
+//{
+//	assert(dest != NULL);
+//	assert(src != NULL);
+//	char* ret = dest;
+//	//拷贝目标字符串到dest指向的空间。
+//	while (*dest++ = *src++)//妙不可言。执行同时利用结果作为循环体的判定。
+//	{
+//		;
+//	}
+//	//返回指向空间的起始地址。
+//	return ret;
+//}
+//
+//int main()
+//{
+//	char arr1[] = "abcdef";
+//	char arr2[] = "ghi";
+//	//用一段代码写出strcpy的运行。
+//	my_strcpy(arr1, arr2);
+//	printf("%s\n", arr1);
+//	return 0;
+//}
+
+int main()
+{
+	char arr1[30] = "hello";
+	char arr2[] = "world";
+	strcat(arr1, arr2);
+	printf("%s\n", arr1);
+	return 0;
+}
