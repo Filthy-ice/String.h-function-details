63d1ebeb7e5c6e1fb2f9574e4896aaee6129a152
Filthy-ice <345134522@qq.com>
2022-07-17 22:34:41 +0800

    07.17 up to date

--- learn.c
+++ learn.c
@@ -28,11 +28,60 @@
 //	return 0;
 //}
 
+//int main()
+//{
+//	char arr1[30] = "hello";
+//	char arr2[] = "world";
+//	strcat(arr1, arr2);
+//	printf("%s\n", arr1);
+//	return 0;
+//}
+
+
+//strstr -- 字符串函数查找的实现
+
+char* my_strstr(const char* p1, const char* p2)
+{
+	assert(p1 && p2);
+	char* S1 = NULL;
+	char* S2 = NULL;
+	char* mark = (char*)p1;
+	if (!*p2)
+	{
+		return (char*)p1;
+	}
+	while (*mark)
+	{
+		S1 = mark;
+		S2 = (char*)p2;
+		while (*S1 && *S2 && (*S1 == *S2))
+		{
+			S1++, S2++;
+		}
+		if (!*S2)
+		{
+			return mark;
+		}
+		else
+			mark++;
+	}
+	return NULL;
+}
+
 int main()
 {
-	char arr1[30] = "hello";
-	char arr2[] = "world";
-	strcat(arr1, arr2);
-	printf("%s\n", arr1);
+	const char* p1 = "abbcdef";
+	const char* p2 = "bcd";
+	//strstr 函数的返回值为 char* 类型
+	char* ret = my_strstr(p1, p2);
+	if (ret == NULL)
+	{
+		printf("字符串不存在\n");
+	}
+	else
+	{
+		printf("%s\n", ret);
+	}
 	return 0;
 }
+
